1e5b458165888bcbfc5ace01d6e187abe869284e
HaddadCSharp <haddad.dadban@outlook.com>
2020-03-15 09:04:05 +0330

    IMDbList

---
+++ IMDbApiLib/ApiLib/IMDbList.cs
@@ -0,0 +1,23 @@
+﻿using IMDbApiLib.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace IMDbApiLib
+{
+    public partial class ApiLib
+    {
+        public async Task<IMDbListData> IMDbListAsync(string id)
+        {
+            try
+            {
+                string url = $"{BaseUrl}/en/API/IMDbList/{_apiKey}/{id}";
+                string json = await Utils.DownloadJsonAsync(url);
+                return await Utils.DownloadObjectAsync<IMDbListData>(url);
+            }
+            catch (Exception ex)
+            {
+                return new IMDbListData() { ErrorMessage = ex.Message };
+            }
+        }
+    }
+}
---
+++ IMDbApiLib/Models/IMDbListData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace IMDbApiLib.Models
+{
+    public class IMDbListData
+    {
+        public string Title { get; set; }
+        public string By { get; set; }
+        public string Created { get; set; }
+        public string Updated { get; set; }
+        public string Description { get; set; }
+
+        public List<IMDbListDataDetail> Items { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+
+    public class IMDbListDataDetail
+    {
+        public string Id { get; set; }
+        public string Index { get; set; }
+        public string Title { set; get; }
+        public string FullTitle { set; get; }
+        public string Year { set; get; }
+        public string Image { get; set; }
+        public string IMDbRating { get; set; }
+        public string IMDbRatingCount { get; set; }
+        public string Description { get; set; }
+    }
+}
